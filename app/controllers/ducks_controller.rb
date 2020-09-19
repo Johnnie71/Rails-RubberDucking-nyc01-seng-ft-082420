371b38f9f734e7d42b6d95237f579645746d9d58
johnnie71 <johnnie.agonz71@gmail.com>
2020-09-19 12:56:11 -0400

    Duck show page and links

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -1,7 +1,28 @@
 class DucksController < ApplicationController
 
+  before_action :find_duck, only: [:show, :create, :edit, :update]
+
   def index
     @ducks = Duck.all
+  end
+
+  def show
+    
+  end
+
+  def edit
+    
+  end
+
+
+  private
+
+  def duck_params
+    params.require(:duck).permit!
+  end
+
+  def find_duck
+    @duck = Duck.find(params[:id])
   end
 
 end
