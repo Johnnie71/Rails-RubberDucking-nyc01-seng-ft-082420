6902df707cba8623ccc8fe0da054f22e6a80bc56
johnnie71 <johnnie.agonz71@gmail.com>
2020-09-19 12:14:16 -0400

    students new page

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,28 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+    @student = Student.new
+  end
+
+  def create
+    student = Student.create(student_params)
+
+    if student.valid?
+      redirect_to student_path(student)
+    else
+      flash[:errors] = student.errors.full_messages
+      redirect_to new_student_path
+    end
+
+  end
+
+
+
+  private
+
+  def student_params
+    params.require(:student).permit!
+  end
+
 end
